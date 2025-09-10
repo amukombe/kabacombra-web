8b37a92a87bf0cd8a433bdc352e5d9e459bbe842
amukombe <amukombe56@gmail.com>
2025-09-10 10:40:56 +0300

    2025-09-10-10-40

--- db/migrate/20250910071408_add_update_payments.rb
+++ db/migrate/20250910071408_add_update_payments.rb
@@ -1,5 +1,6 @@
 class AddUpdatePayments < ActiveRecord::Migration[7.2]
   def change
+    remove_column :payments, :recipient_type, :string
     add_reference :payments, :recipient, polymorphic: true, null: false
     add_reference :payments, :expense_category, foreign_key: true, null: true
     add_column :payments, :cheque_number, :string, null: true
